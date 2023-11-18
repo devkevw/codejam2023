8c5371ae7155c765f110373a04b4e2cf04569ef7
zuhayr <zuhayrmahmood@yahoo.ca>
2023-11-18 01:04:20 -0500

    Committing the Navbar

--- ootd/lib/main.dart
+++ ootd/lib/main.dart
@@ -81,10 +81,26 @@ class _MyHomePageState extends State<MyHomePage> {
         // TRY THIS: Try changing the color here to a specific color (to
         // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
         // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        backgroundColor: Colors.red,
         // Here we take the value from the MyHomePage object that was created by
         // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.add_box),
+            label: 'Add Fit',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person_add_alt_1),
+            label: 'Add Friend',
+          )
+        ],
       ),
       body: Center(
         // Center is a layout widget. It takes a single child and positions it
